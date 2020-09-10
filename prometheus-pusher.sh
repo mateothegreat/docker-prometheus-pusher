310db52539503a97cb94d4221613c738cc82cac8
Matthew Davis <matthew@matthewdavis.io>
2020-09-10 19:39:53 -0400

    bump

--- prometheus-pusher.sh
+++ prometheus-pusher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -s localhost:9100/metrics | curl --data-binary @- $PUSHGATEWAY_SERVER/metrics/job/node-exporter/instance/$NODE_NAME
+curl -s prometheus:9100/metrics | curl --data-binary @- $PUSHGATEWAY_SERVER/metrics/job/node-exporter/instance/$NODE_NAME
